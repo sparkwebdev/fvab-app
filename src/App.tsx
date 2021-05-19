9691ed8fb603819254af072963c2932de56a790b
Steven Park <info@stevenpark.co.uk>
2021-05-19 13:48:48 +0100

    App Tabs — first draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route } from "react-router-dom";
 import {
   IonApp,
   IonIcon,
@@ -7,62 +7,92 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { home, bicycle, heart, people, analytics } from "ionicons/icons";
+import HomePage from "./pages/HomePage";
+import MyMapPage from "./pages/MyMapPage";
+import StudiosPage from "./pages/StudiosPage";
+import ArtistsPage from "./pages/ArtistsPage";
+import NewsPage from "./pages/NewsPage";
+import AboutPage from "./pages/AboutPage";
+import ArtCyclePage from "./pages/ArtCyclePage";
+import StudioEntryPage from "./pages/StudioEntryPage";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
+import "./theme/custom-variables.css";
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path="/home">
+            <HomePage />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
+          <Route exact path="/my-map">
+            <MyMapPage />
           </Route>
-          <Route path="/tab3">
-            <Tab3 />
+          <Route exact path="/studios">
+            <StudiosPage />
+          </Route>
+          <Route path="/artists">
+            <ArtistsPage />
+          </Route>
+          <Route path="/art-cycle">
+            <ArtCyclePage />
+          </Route>
+          <Route path="/about">
+            <AboutPage />
+          </Route>
+          <Route path="/news">
+            <NewsPage />
+          </Route>
+          {/* Dynamic pages */}
+          <Route exact path="/studio/:id">
+            <StudioEntryPage />
           </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/home" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+          <IonTabButton tab="home" href="/home">
+            <IonIcon icon={home} />
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+          <IonTabButton tab="studios" href="/studios">
+            <IonIcon icon={analytics} />
+            <IonLabel>Studios</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+          <IonTabButton tab="artists" href="/artists">
+            <IonIcon icon={people} />
+            <IonLabel>Artists</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="mymap" href="/my-map">
+            <IonIcon icon={heart} />
+            <IonLabel>My Map</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="artcycle" href="/art-cycle">
+            <IonIcon icon={bicycle} />
+            <IonLabel>ArtCycle</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
