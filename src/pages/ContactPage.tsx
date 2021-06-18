55e93aaa5dcd113bf68e49f84fd060af118edbc0
Steven Park <info@stevenpark.co.uk>
2021-06-18 10:00:00 +0100

    Contact page and News Page W.I.P.

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,12 +1,60 @@
-import { IonContent, IonPage } from "@ionic/react";
+import {
+  IonContent,
+  IonPage,
+  IonCard,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonIcon,
+} from "@ionic/react";
 import "./ContactPage.css";
 import PageHeader from "../components/PageHeader";
+import { logoFacebook, logoTwitter } from "ionicons/icons";
 
 const ContactPage: React.FC = () => {
   return (
     <IonPage>
       <PageHeader title="Contact" />
-      <IonContent fullscreen></IonContent>
+      <IonContent fullscreen>
+        <IonCard color="light" className="ion-text-center">
+          <IonCardContent>
+            <p>
+              <strong>Email us:</strong>
+              <br />
+              <strong>
+                <a href="info:avril@forthvalleyartbeat.com">
+                  info@forthvalleyartbeat.com
+                </a>
+              </strong>
+            </p>
+            <hr />
+            <IonGrid>
+              <strong>Follow us:</strong>
+              <IonRow className="ion-justify-content-center">
+                <IonCol size="3">
+                  <a
+                    href="https://www.facebook.com/forthvalleyartbeat/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <IonIcon icon={logoFacebook} color="primary" size="large" />
+                  </a>
+                </IonCol>
+                <IonCol size="3">
+                  <a
+                    href="https://twitter.com/fvab_"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <IonIcon icon={logoTwitter} color="primary" size="large" />
+                  </a>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
     </IonPage>
   );
 };
--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,25 +1,68 @@
-import { IonContent, IonPage } from "@ionic/react";
+import {
+  IonContent,
+  IonPage,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonLoading,
+  IonItem,
+  IonList,
+} from "@ionic/react";
 import "./NewsPage.css";
 import PageHeader from "../components/PageHeader";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 //
 
 const NewsPage: React.FC = () => {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetch("https://forthvalleyartbeat.com/?page_id=374&datatype=news")
       .then((res) => res.json())
       .then((data) => {
-        // this.setState({ todos: data });
-        console.log(data);
+        setNews(data);
+        setLoading(false);
       })
-      .catch(console.log);
+      .catch((e) => {
+        console.log("Error getting news", e);
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) {
+    return <IonLoading isOpen message={"Please wait..."} />;
+  }
 
   return (
     <IonPage>
-      <PageHeader title="My Map" />
-      <IonContent fullscreen></IonContent>
+      <PageHeader title="News" />
+      <IonContent fullscreen>
+        {news && news.length > 0 ? (
+          <IonList>
+            {news.map((entry: any) => {
+              return (
+                <IonItem key={entry.id}>
+                  <h2>{entry.title}</h2>
+                  <p>{entry.date}</p>
+                  {entry.content}
+                </IonItem>
+              );
+            })}
+          </IonList>
+        ) : (
+          <IonCard color="light" className="ion-text-center">
+            <IonCardHeader>
+              <IonCardTitle>Nothing found</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <p>Sorry, no news items yet.</p>
+            </IonCardContent>
+          </IonCard>
+        )}
+      </IonContent>
     </IonPage>
   );
 };
